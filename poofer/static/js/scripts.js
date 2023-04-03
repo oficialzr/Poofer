8b84edc91d95ff7fb7eb1b845f2456375c95a80e
oficialzr <49753192+oficialzr@users.noreply.github.com>
2023-04-03 11:49:07 +0300

    Add

--- poofer/static/js/scripts.js
+++ poofer/static/js/scripts.js
@@ -3,3 +3,15 @@ window.addEventListener('scroll', function() {
     body = document.getElementsByTagName('body')[0]
     navbar = document.getElementById('navbar')
 })
+
+
+
+function scroller() {
+    products = ['rx200.jpg', 'wrp200.jpg', 'stick-vs.jpg', 'amaze.jpg']
+    cards = document.getElementsByClassName('models-img')
+    for (let card in cards) {
+        cards[card].style.backgroundImage = `url('../images/main/${products[card]}')`
+    }
+}
+
+scroller()
